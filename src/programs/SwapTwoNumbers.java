53bd5a517144efdfbd7a357992911ac96e70961b
nageshkumar <nageshkumar.k@skill-mine.com>
2025-08-31 23:17:40 +0530

    added on program

---
+++ src/programs/SwapTwoNumbers.java
@@ -0,0 +1,15 @@
+package programs;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a=60;
+        int b=30;
+        int temp;
+
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("value of A"+ a);
+        System.out.println("value of B "+ b);
+    }
+}
